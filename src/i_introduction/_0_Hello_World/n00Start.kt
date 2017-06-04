50be2af3fc0389382a1d695f3734ffb50a9175c5
fada21 <lukasz.czaplicki@gmail.com>
2017-06-05 00:29:32 +0100

    solve koans

--- src/i_introduction/_0_Hello_World/n00Start.kt
+++ src/i_introduction/_0_Hello_World/n00Start.kt
@@ -20,10 +20,10 @@ fun todoTask0(): Nothing = TODO(
 
         Let's start! Make the function 'task0' return "OK". Note that you can return expression directly.
     """,
-    documentation = doc0(),
-    references = { task0(); "OK" }
+        documentation = doc0(),
+        references = { task0(); "OK" }
 )
 
 fun task0(): String {
-    return todoTask0()
+    return "OK"
 }
